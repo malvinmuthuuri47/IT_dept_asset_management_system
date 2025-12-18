46007d7a77486fd810cf9178b1578f96db6c5664
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-12-18 14:14:52 +0300

    Updated the ITAM system to include custom interfaces for the admin to CRUD users, employees, and Computers

--- hardware_mgmnt_system/assets/admin.py
+++ hardware_mgmnt_system/assets/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Employee, Department, Computer
+from .models import Employee, Department, Computer, ComputerInfo, Role
 from django.contrib.auth.models import User
 
 # Register your models here.
@@ -8,6 +8,7 @@ from django.contrib.auth.models import User
 class EmployeeInline(admin.StackedInline):
     model = Employee
     fields = ['department', 'date_of_birth', 'gender', 'role']
+    autocomplete_fields = ['role']
     extra = 0
 
 # extend User admin to show Employee Inline
@@ -17,8 +18,19 @@ class CustomUserAdmin(UserAdmin):
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
 
+class RoleInline(admin.TabularInline):
+    model = Role
+    extra = 1
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ['name', 'department']
+    list_filter = ['department']
+    search_fields = ['name']
+
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
+    inlines = [RoleInline]
     list_display = ['name']
 
 @admin.register(Employee)
@@ -27,11 +39,17 @@ class EmployeeAdmin(admin.ModelAdmin):
     list_filter = ['department', 'role']
     raw_id_fields = ['user']
 
+class ComputerInfoInline(admin.StackedInline):
+    model = ComputerInfo
+    fields = ['brand', 'name', 'screen_type', 'screen_aspect_ratio', 'memory_size', 'storage_size', 'storage_type']
+    extra = 1
+
 @admin.register(Computer)
 class ComputerAdmin(admin.ModelAdmin):
-    list_display = ['comp_name', 'asset_tag', 'department', 'current_user', 'status']
+    inlines = [ComputerInfoInline]
+    list_display = ['computer_name', 'asset_tag', 'department', 'current_user', 'status']
     list_filter = ['department']
     # raw_id_fields = ['current_user']
     # search_fields = ['computer_name', 'asset_tag']
     readonly_fields = ['asset_tag']
-    fields = ['comp_name', 'department', 'asset_tag', 'status']
+    fields = ['computer_name', 'department', 'asset_tag', 'status']
--- hardware_mgmnt_system/assets/models.py
+++ hardware_mgmnt_system/assets/models.py
@@ -9,17 +9,28 @@ class Department(models.Model):
     def __str__(self):
         return self.name
 
+class Role(models.Model):
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='roles')
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        unique_together = ['department', 'name']
+    
+    def __str__(self):
+        return f"{self.department.name} - {self.name}"
+
 class Employee(models.Model):
     GENDER_CHOICES = [
         ('', '-------'),
         ('M', 'Male'),
         ('F', 'Female'),
     ]
+
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="employee_profile")
     department = models.ForeignKey(Department, on_delete=models.PROTECT, related_name="employees")
     date_of_birth = models.DateField(null=True, blank=True)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=False)
-    role = models.CharField(max_length=50, blank=True)
+    role = models.ForeignKey(Role, on_delete=models.PROTECT, related_name="employees")
 
     def __str__(self):
         return self.user.get_username()
@@ -28,18 +39,18 @@ class Computer(models.Model):
     STATUS_CHOICES = [
         ('Issued', 'In Use'),
         ('In Repair', 'In Repair'),
-        ('Spare', 'In Inventory'),
+        ('Inventory', 'In Inventory'),
         ('Faulty', 'Faulty')
     ]
-    comp_name = models.CharField(max_length=100, null=True)
-    asset_tag = models.CharField(max_length=100, unique=True, editable=False)
+    computer_name = models.CharField(max_length=100, null=True)
+    asset_tag = models.CharField(max_length=100, unique=True, editable=False,)
     current_user = models.OneToOneField(Employee, on_delete=models.SET_NULL, null=True, blank=True, related_name="computer")
     department = models.ForeignKey(Department, on_delete=models.PROTECT, related_name="computers")
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, blank=False)
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, blank=False, default='Inventory')
 
     def generate_asset_tag(self):
-        if self.comp_name and self.department:
-            name_slug = slugify(self.comp_name).upper()
+        if self.computer_name and self.department:
+            name_slug = slugify(self.computer_name).upper()
             dept_slug = slugify(self.department.name).upper()
 
             base_tag = f"{name_slug}-{dept_slug}"
@@ -50,7 +61,7 @@ class Computer(models.Model):
             if max_num:
                 try:
                     last_num = int(max_num.split('-')[-1])
-                    nex_num = last_num + 1
+                    next_num = last_num + 1
                 except (ValueError, IndexError):
                     next_num = 1
             else:
@@ -69,12 +80,54 @@ class Computer(models.Model):
 
 
 class ComputerInfo(models.Model):
+    ASPECT_CHOICES = [
+        ('16:9', '16:9'),
+        ('16:10', '16:10'),
+        ('5:4', '5:4'),
+        ('4:3', '4:3'),
+    ]
+
+    SCREEN_CHOICES = [
+        ('TN', 'TN'),
+        ('VA', 'VA'),
+        ('IPS', 'IPS'),
+        ('OLED', 'OLED'),
+        ('Mini-LED', 'Mini-LED'),
+        ('QLED', 'QLED'),
+        ('LCD', 'LCD'),
+    ]
+
+    MEMORY_CHOICES = [
+        (4, "4 GB"),
+        (8, "8 GB"),
+        (12, "12 GB"),
+        (16, "16 GB"),
+        (24, "24 GB"),
+        (32, "32 GB"),
+        (64, "64 GB"),
+        (96, "96 GB"),
+        (128, "128 GB"),
+    ]
+    
+    STORAGE_TYPE_CHOICES = [
+        ('HDD', 'Hard Disk Storage (HDD)'),
+        ('SSD', 'Solid State Storage (SSD)')
+    ]
+
+    STORAGE_SIZE_CHOICES = [
+        ('256 GB', '256 GB'),
+        ('512 GB', '512 GB'),
+        ('1 TB', '1 TB'),
+        ('2 TB', '2 TB'),
+    ]
     computer = models.OneToOneField(Computer, on_delete=models.CASCADE, primary_key=True, related_name="info")
     brand = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
-    screen_type = models.CharField(max_length=50)
-    memory_size = models.PositiveIntegerField(help_text="RAM in GB")
-    storage_type = models.CharField(max_length=50)
+    screen_type = models.CharField(max_length=12, choices=SCREEN_CHOICES)
+    screen_aspect_ratio = models.CharField(max_length=10, choices=ASPECT_CHOICES)
+    memory_size = models.PositiveIntegerField(help_text="RAM in GB", choices=MEMORY_CHOICES)
+    storage_type = models.CharField(max_length=50, choices=STORAGE_TYPE_CHOICES)
+    storage_size = models.CharField(max_length=50, choices=STORAGE_SIZE_CHOICES)
 
     def __str__(self):
         return f"{self.brand} {self.name} ({self.computer.asset_tag})"
